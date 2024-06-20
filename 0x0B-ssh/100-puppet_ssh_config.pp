cb3bc211859c98a6c30edddaf6b660afc906eb2c
Justicesibisi <djmangsa@gmail.com>
2024-06-20 13:47:48 -0700

    ssh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,8 @@
+# modules/ssh/templates/client_config.erb
+
+Host 498833-web-01
+    HostName 100.25.220.131
+    User ubuntu
+    IdentityFile /home/ubuntu/.ssh/school
+    PasswordAuthentication no
+
